2ed73d022e414f86435e5cfd42627d02d6148503
Michael Fanger <mike.fanger@gmail.com>
2019-07-02 23:31:37 -0400

    Add functions for update and delete user

--- apps/lynx_list/lib/lynx_list/accounts/accounts.ex
+++ apps/lynx_list/lib/lynx_list/accounts/accounts.ex
@@ -1,5 +1,7 @@
 defmodule LynxList.Accounts do
   import Ecto.Query, only: [from: 2]
+  alias Ecto.Changeset
+
   alias LynxList.Repo
   alias LynxList.Accounts.{User, Credentials}
 
@@ -31,5 +33,17 @@ defmodule LynxList.Accounts do
         where: c.github_id == ^id
 
     Repo.one!(query)
+  end
+
+  @spec update_user(%User{}, map) :: {:error, Changeset.t()} | {:ok, %User{}}
+  def update_user(%User{} = user, attrs) do
+    user
+    |> User.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @spec delete_user(%User{}) :: {:error, Changeset.t()} | {:ok, %User{}}
+  def delete_user(%User{} = user) do
+    Repo.delete(user)
   end
 end
