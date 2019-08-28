234766f8dd40175d48fcd27da0d8058bb840b86a
Michael Fanger <mike.fanger@gmail.com>
2019-08-27 22:43:01 -0400

    Add 404 test on show route

---
+++ test/lynx_list_web/controllers/link_record_controller_test.exs
@@ -0,0 +1,32 @@
+defmodule LynxListWeb.LinkRecordControllerTest do
+  use LynxListWeb.ConnCase, async: true
+
+  alias Ecto.UUID
+  alias LynxList.Fixtures
+
+  setup do
+    user = Fixtures.user()
+    link_record = Fixtures.link_record(user)
+    {:ok, link_record: link_record, user: user}
+  end
+
+  # test "GET /api/link-record/<id> should return LinkRecord with the provided id", %{
+  #   link_record: link_record
+  # } do
+  #   conn =
+  #     build_conn()
+  #     |> get("/api/link-records/#{link_record.id}")
+
+  #   IO.inspect(conn)
+  # end
+
+  @tag :only
+  test "GET /api/link-record<id> should return a 404 when a LinkRecord with the provided id does not exist" do
+    conn =
+      build_conn()
+      |> get("/api/link-records/#{UUID.generate()}")
+
+    assert json_response(conn, 404) ==
+             render_json(LynxListWeb.ErrorView, "404.json")
+  end
+end
