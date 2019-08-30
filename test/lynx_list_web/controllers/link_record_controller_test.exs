dd4942a0e60558e13cf7426c0b8947a0e0badab5
Michael Fanger <mike.fanger@gmail.com>
2019-08-30 17:26:27 -0400

    Add test for invalid input

--- test/lynx_list_web/controllers/link_record_controller_test.exs
+++ test/lynx_list_web/controllers/link_record_controller_test.exs
@@ -11,7 +11,8 @@ defmodule LynxListWeb.LinkRecordControllerTest do
     {:ok, link_record: link_record, user: user}
   end
 
-  describe "POST /api/link-record/" do
+  describe "POST /api/link-records/" do
+    @url "/api/link-records/"
     @valid_attrs %{
       "description" => "Some description",
       "private" => false,
@@ -25,7 +26,7 @@ defmodule LynxListWeb.LinkRecordControllerTest do
       json_response =
         user
         |> create_authed_conn()
-        |> post("/api/link-records/", post_body)
+        |> post(@url, post_body)
         |> json_response(200)
 
       assert Map.keys(json_response) |> Enum.count() == 1
@@ -46,9 +47,27 @@ defmodule LynxListWeb.LinkRecordControllerTest do
       response =
         user
         |> create_authed_conn()
-        |> post("/api/link-records/", post_body)
+        |> post(@url, post_body)
 
       assert response.status == 409
+    end
+
+    test "should return a 400 when a user submits invalid data", %{user: user} do
+      invalid_data = [
+        Map.drop(@valid_attrs, ["url"]),
+        Map.put(@valid_attrs, "private", "foo"),
+        Map.put(@valid_attrs, "title", true)
+      ]
+
+      Enum.each(invalid_data, fn post_body ->
+        response =
+          user
+          |> create_authed_conn()
+          |> post(@url, post_body)
+          |> json_response(400)
+
+        assert response == render_json(LynxListWeb.ErrorView, "invalid_input_error.json")
+      end)
     end
   end
 
